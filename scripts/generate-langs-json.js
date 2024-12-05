11c1ae8a6dbad7626376e365102c368888692045
Lemon-Juiced <47193272+Lemon-Juiced@users.noreply.github.com>
2024-12-04 21:27:29 -0500

    This injects custom colors into the languageColors.json file. Rudimentary fix to #1164

--- scripts/generate-langs-json.js
+++ scripts/generate-langs-json.js
@@ -3,26 +3,35 @@ import fs from "fs";
 import jsYaml from "js-yaml";
 
 const LANGS_FILEPATH = "./src/common/languageColors.json";
+const CUSTOM_LANGS_FILEPATH = "./src/common/customLanguageColors.json";
 
-//Retrieve languages from github linguist repository yaml file
-//@ts-ignore
+// Retrieve languages from GitHub linguist repository YAML file
 axios
   .get(
     "https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml",
   )
   .then((response) => {
-    //and convert them to a JS Object
+    // Convert them to a JS Object
     const languages = jsYaml.load(response.data);
 
     const languageColors = {};
 
-    //Filter only language colors from the whole file
+    // Filter only language colors from the whole file
     Object.keys(languages).forEach((lang) => {
       languageColors[lang] = languages[lang].color;
     });
 
-    //Debug Print
-    //console.dir(languageColors);
+    // Read custom language colors from JSON file
+    const customLanguageColors = JSON.parse(
+      fs.readFileSync(CUSTOM_LANGS_FILEPATH, "utf8"),
+    );
+
+    // Merge custom colors with default colors
+    Object.keys(customLanguageColors).forEach((lang) => {
+      languageColors[lang] = customLanguageColors[lang];
+    });
+
+    // Write the merged language colors to the output file
     fs.writeFileSync(
       LANGS_FILEPATH,
       JSON.stringify(languageColors, null, "    "),
