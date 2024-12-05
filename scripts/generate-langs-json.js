b880c68f92e0dc53a4523dcac8e8118e0681ab9c
Lemon-Juiced <47193272+Lemon-Juiced@users.noreply.github.com>
2024-12-04 23:25:35 -0500

    Pulling from new source altogether, not linguist

--- scripts/generate-langs-json.js
+++ scripts/generate-langs-json.js
@@ -3,39 +3,24 @@ import fs from "fs";
 import jsYaml from "js-yaml";
 
 const LANGS_FILEPATH = "./src/common/languageColors.json";
-const CUSTOM_LANGS_FILEPATH = "./src/common/customLanguageColors.json";
 
-// Read custom language colors from JSON file
-const customLanguageColors = JSON.parse(
-  fs.readFileSync(CUSTOM_LANGS_FILEPATH, "utf8"),
-);
+//Retrieve languages from github linguist repository yaml file
+//@ts-ignore
+axios.get("https://lemonjuice.dev/better_linguist.yml").then((response) => {
+  //and convert them to a JS Object
+  const languages = jsYaml.load(response.data);
 
-const languageColors = { ...customLanguageColors };
+  const languageColors = {};
 
-// Retrieve languages from GitHub linguist repository YAML file
-axios
-  .get(
-    "https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml",
-  )
-  .then((response) => {
-    // Convert them to a JS Object
-    const languages = jsYaml.load(response.data);
-
-    // Add colors from Linguist only if they do not conflict with custom colors
-    Object.keys(languages).forEach((lang) => {
-      if (!languageColors[lang]) {
-        languageColors[lang] = languages[lang].color;
-      }
-    });
-
-    // Write the merged language colors to the output file
-    fs.writeFileSync(
-      LANGS_FILEPATH,
-      JSON.stringify(languageColors, null, "    "),
-    );
-
-    console.log("Language colors have been written to", LANGS_FILEPATH);
-  })
-  .catch((error) => {
-    console.error("Error fetching or processing language colors:", error);
+  //Filter only language colors from the whole file
+  Object.keys(languages).forEach((lang) => {
+    languageColors[lang] = languages[lang].color;
   });
+
+  //Debug Print
+  //console.dir(languageColors);
+  fs.writeFileSync(
+    LANGS_FILEPATH,
+    JSON.stringify(languageColors, null, "    "),
+  );
+});
